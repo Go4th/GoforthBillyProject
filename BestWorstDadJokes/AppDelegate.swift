850a543c4340eb71e9bb623a42e02538bc82ecd8
Student <student@amarillos-macbook-pro.local>
2019-03-02 01:01:22 -0600

    Latest commit with new ui display from database and placeholder logo.

--- BestWorstDadJokes/AppDelegate.swift
+++ BestWorstDadJokes/AppDelegate.swift
@@ -8,90 +8,21 @@
 
 import UIKit
 import Firebase
-import FirebaseUI
-import FirebaseAuth
-
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Use Firebase library to configure APIs
-//        FirebaseApp.configure()
-//        var ref: DatabaseReference!
-//
-//        ref = Database.database().reference()
-//
-//        let db = Database.database().reference().child("jokes").queryOrdered(byChild: "PostUser")
-//        db.observe(.value, with:{ (snapshot: DataSnapshot) in
-//            for snap in snapshot.children {
-//                print((snap as! DataSnapshot).value)
-////                var name = snap.value("PostUser")
-//                
-//
-//            }
-//        })
-//        ref.child("jokes").observeSingleEvent(of: .value , with: {(snapshot) in
-//                let value = snapshot.value as? NSDictionary
-//            print(value)
-//            let joke1 = value?[0]
-//            print(joke1)
-////            print(user)
-//        })
-//        
-//            ref.observe(.value, with: { (snapshot) in
-//                for child in snapshot.children { //even though there is only 1 child
-//                    let snap = child as! DataSnapshot
-//                    let dict = snap.value as? NSDictionary
-//                    for (joke, item) in dict ?? [:] {
-////                        let userName = joke["PostUser"] as? String
-//                        print(joke)
-//                        print(item)
-//                        
-//                        
-//                       
-//                      
-//                    }
-////                    let name = dict?["PostUser"] as? String ?? ""
-////                    let setup = dict?["Setup"] as? String ?? ""
-////                    print(name, setup)
-//                }
-//            })
+        FirebaseApp.configure()
         // Override point for customization after application launch.
-//        let authUI = FUIAuth.defaultAuthUI()
-//        // You need to adopt a FUIAuthDelegate protocol to receive callback
-//        authUI?.delegate = self as? FUIAuthDelegate
-//        let providers: [FUIAuthProvider] = [
-//            FUIGoogleAuth()
-//        ]
-//        authUI?.providers = providers
-//
-//        let authViewController = authUI?.authViewController()
-        
-        
         return true
     }
-//    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
-//        // handle user and error as necessary
-//        print(error)
-//    }
-//    private func application(_ app: UIApplication, open url: URL,
-//                             options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-//        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
-//        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
-//            return true
-//        }
-//        // other URL handling goes here.
-//        return false
-//    }
+
     func applicationWillResignActive(_ application: UIApplication) {
-        
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
--- BestWorstDadJokes/ViewController.swift
+++ BestWorstDadJokes/ViewController.swift
@@ -10,53 +10,81 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //Setup Database Reference
+//    FirebaseApp.configure()
+//    var ref = Database.database().reference()
+      @IBOutlet weak var TableViewOutlet: UITableView!
+    
+
+    
+    var dict : [String: AnyObject] = [:]
+    var jokes = [Jokes]()
+    var placeholderDict : [String: AnyObject] = [:]
+    private let myArray: NSArray = ["First","Second","Third"]
+    private var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var ref = Database.database().reference()
+                ref.observe(.value, with: { (snapshot) in
+                    for child in snapshot.children { //even though there is only 1 child
+                        let snap = child as! DataSnapshot
+                        //                let dict = snap.value as? NSDictionary
+                        self.dict = snap.value as! [String: AnyObject]
+                        for (joke, item) in self.dict ?? [:] {
+                            let myJokeCollection = Jokes(fromDict: item as? [String : AnyObject] ?? self.placeholderDict);
+                            self.jokes.append(myJokeCollection)
+                            print(myJokeCollection)
+                            print(myJokeCollection.postUser)
+                            self.myTableView.reloadData()
+                        }
+                    }
+                })
+        
+        //Below is intial example
+        
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = 400
+//        let displayHeight: CGFloat = self.view.frame.height
+        let displayHeight: CGFloat = 400
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: 300, width: displayWidth, height: displayHeight - barHeight))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "JokeCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+//        print("Value: \(myArray[indexPath.row])")
+        print("Value: \(jokes[indexPath.row].setup)")
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return jokes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JokeCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(jokes[indexPath.item].setup)"
+        return cell
     }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        FirebaseApp.configure()
-        var ref = Database.database().reference()
-        
-        ref.observe(.value, with: { (snapshot) in
-            for child in snapshot.children { //even though there is only 1 child
-                let snap = child as! DataSnapshot
-                //                let dict = snap.value as? NSDictionary
-                guard let dict = snap.value as? [String: AnyObject] else {continue}
-                for (joke, item) in dict ?? [:] {
-                    let myJokeCollection = Jokes(fromDict: item as! [String : AnyObject]);
-                    print(myJokeCollection)
-                    print(myJokeCollection.postUser)
-                    }
-                
-                
-            }
-        })
-        
-       
-    }
-
-
+  
 }
-struct Jokes {
-    var postUser: String?
-    var punchline: String?
-    var rating: Int?
-    var setup: String?
+ class Jokes {
+    var postUser: String
+    var punchline: String
+    var rating: Int
+    var setup: String
     
     init(fromDict dict:[String: AnyObject]) {
-        self.postUser = dict["PostUser"] as? String
-        self.punchline = dict["Punchline"] as? String
-        self.rating = dict["Rating"] as? Int
-        self.setup = dict["Setup"] as? String
+        self.postUser = dict["PostUser"] as? String ?? ""
+        self.punchline = dict["Punchline"] as? String ?? ""
+        self.rating = dict["Rating"] as? Int ?? 0
+        self.setup = dict["Setup"] as? String ?? ""
         
     }
 }
